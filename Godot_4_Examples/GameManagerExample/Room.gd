4baade3db326ed6e4537b02d65c53c236861bcbc
Andrew Thawley <58050814+athawley@users.noreply.github.com>
2023-05-30 14:03:37 +1200

    Room changing

---
+++ Godot_4_Examples/GameManagerExample/Room.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@onready var target = $Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$Camera3D.look_at(target.position, Vector3.UP)
+	pass
---
+++ Godot_4_Examples/GameManagerExample/Transporter.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+@export var TargetScene: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_3d_body_entered(body):
+	print(str(body, " entered"))
+	
+	var scene_location = "res://GameManagerExample/Room" + TargetScene + ".tscn"
+	get_tree().change_scene_to_file(scene_location)
+	pass # Replace with function body.
