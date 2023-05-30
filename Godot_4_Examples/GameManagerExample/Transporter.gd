968efa453bb9bde3b230e317689c50ddfcccbf35
Andrew Thawley <58050814+athawley@users.noreply.github.com>
2023-05-30 14:57:59 +1200

    Update Transporter.gd

--- Godot_4_Examples/GameManagerExample/Transporter.gd
+++ Godot_4_Examples/GameManagerExample/Transporter.gd
@@ -17,4 +17,6 @@ func _on_area_3d_body_entered(body):
 	
 	var scene_location = "res://GameManagerExample/Room" + TargetScene + ".tscn"
 	get_tree().change_scene_to_file(scene_location)
+	
+	#get_tree().change_scene_to_file("res://GameManagerExample/RoomD.tscn")
 	pass # Replace with function body.
