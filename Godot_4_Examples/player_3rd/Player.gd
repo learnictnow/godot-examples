2e2bb66a37c1b1decee43016008338cf199e513d
Andrew Thawley <58050814+athawley@users.noreply.github.com>
2023-06-02 10:04:38 +1200

    Model Added

--- Godot_4_Examples/player_3rd/Player.gd
+++ Godot_4_Examples/player_3rd/Player.gd
@@ -7,7 +7,7 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-var bullet_scene = preload("res://bullet.tscn")
+var bullet_scene = preload("res://player_twin_stick/bullet.tscn")
 
 func _physics_process(delta):	
 
