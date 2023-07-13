ab87a7ef72d69c974f6ba5f591627bc3db58681d
Andrew Thawley <thawleya@staff.cbhs.school.nz>
2023-07-13 20:05:32 +1200

    Custom icon added
    Created build

--- Godot_4_Examples/ui.gd
+++ Godot_4_Examples/ui.gd
@@ -13,40 +13,44 @@ func _process(delta):
 
 func _on_button_3_rd_person_pressed():
 	print("3rd")
-	get_tree().change_scene_to_file("res://player_3rd/Level_3rd_person.tscn")
+	get_tree().change_scene_to_file("res://examples/3D/player_3rd/Level_3rd_person.tscn")
 	pass # Replace with function body.
 
 
 func _on_button_fps_pressed():
 	print("FPS")
-	
+	get_tree().change_scene_to_file("res://examples/3D/player_fps/Level_fps.tscn")
 	pass # Replace with function body.
 
 
 func _on_button_space_flight_pressed():
 	print("Space")
-	get_tree().change_scene_to_file("res://player_space/Level_space.tscn")
+	get_tree().change_scene_to_file("res://examples/3D/player_space/Level_space.tscn")
 	pass # Replace with function body.
 
 
 func _on_button_car_pressed():
 	print("Car")
-	get_tree().change_scene_to_file("res://player_vehicle/Level_vehicle.tscn")
+	get_tree().change_scene_to_file("res://examples/3D/player_vehicle/Level_vehicle.tscn")
 	pass # Replace with function body.
 
 
 func _on_button_point_and_click_pressed():
 	print("RTS and Navmesh movement")
-	get_tree().change_scene_to_file("res://player_point_and_click_movement/Level_Town.tscn")
+	get_tree().change_scene_to_file("res://examples/3D/player_point_and_click_movement/Level_Town.tscn")
 	pass # Replace with function body.
 
 
 func _on_button_2d_platformer_pressed():
 	print("2D Platformer")
-	get_tree().change_scene_to_file("res://player_2d_platformer/leve_2d_platformer.tscn")
+	get_tree().change_scene_to_file("res://examples/2D/player_2d_platformer/leve_2d_platformer.tscn")
 	pass # Replace with function body.
 
 
 func _on_button_3d_direction_movement_pressed():
-	get_tree().change_scene_to_file("res://3D_Rotate_In_Direction/World.tscn")
+	get_tree().change_scene_to_file("res://examples/3D/3D_Rotate_In_Direction/World.tscn")
+	pass # Replace with function body.
+	
+func _on_button_test_environment_pressed():
+	get_tree().change_scene_to_file("res://levels/test_environment/test_environment.tscn")
 	pass # Replace with function body.
