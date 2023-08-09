8cf7b8f5dd580103c7dae3f18a0637a783122672
Andrew Thawley <58050814+athawley@users.noreply.github.com>
2023-08-09 14:56:05 +1200

    Physics Rigidbody Stand on Object and Move it Test

--- Godot_4_Examples/levels/test_environment/Player.gd
+++ Godot_4_Examples/levels/test_environment/Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const SPRINT_MULTIPLYER = 2.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 10
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
